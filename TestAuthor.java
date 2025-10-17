e68b4dc4ac47ff7cb3e5a270e18fd9f124a6aa3b
vietquocGH <vietdang.26012008@gmail.com>
2025-10-17 10:55:29 +0700

    bai chuong 2

---
+++ TestAuthor.java
@@ -0,0 +1,11 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm');
+        System.out.println(ahTeck);  // Test toString()
+        ahTeck.setEmail("paulTan@nowhere.com");  // Test setter
+        System.out.println("name is: " + ahTeck.getName());
+        System.out.println("email is: " + ahTeck.getEmail());   // Test getter
+        System.out.println("gender is: " + ahTeck.getGender());
+
+    }
+}
